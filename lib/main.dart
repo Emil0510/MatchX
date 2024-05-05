0e71105099dd80ea3251f3ab62ddcfe2c5605ae9
Emil0510 <emilabdurahmanli05@gmail.com>
2024-05-05 12:13:08 +0400

    problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_app/pages/splash.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_app/utils/NavigatorObserver.dart';
 import 'firebase_options.dart';
